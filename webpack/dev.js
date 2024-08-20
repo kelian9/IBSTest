08079d22de99a280dc1af7f2a01894a306c6379e
Aleksandr Karibov <karibov3194@gmail.com>
2024-08-20 05:49:57 +0300

    allow all hosts in wepack dev config

--- webpack/dev.js
+++ webpack/dev.js
@@ -1,19 +1,20 @@
 const { merge } = require("webpack-merge");
 const base = require("./base.js");
 const webpack = require("webpack");
-  
+
 module.exports = merge(base, {
-  mode: "development",
-  devtool: "inline-source-map",
-  devServer: {
-    historyApiFallback: true,
-    port: 8080,
-    hot: true,
-    open: true,
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-        "process.env.NODE_ENV": JSON.stringify("development"),
-    }),
-  ],
+	mode: "development",
+	devtool: "inline-source-map",
+	devServer: {
+		historyApiFallback: true,
+		port: 8080,
+		hot: true,
+		open: true,
+		allowedHosts: 'all',
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			"process.env.NODE_ENV": JSON.stringify("development"),
+		}),
+	],
 });
